04a45d3aee082e4b0125d594c1ebdedbfce67c65
AleksanovDenis <Aleksanov.Denis@gmail.com>
2022-07-12 18:50:41 +0000

    feat: add functionality for element.sh when an argument doesn't exist in the database

--- element.sh
+++ element.sh
@@ -8,19 +8,27 @@ else
   #if argument is a atomic number
   if [[ $1 =~ ^[0-9]+$ ]]
   then
+    #get atomic number, symbol, name from db
     ATOMIC_NUMBER=$1
     SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
     NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
   else
-    #if argument is a symbol or name
+    #if argument is a symbol or name, get atomic number, symbol, name from db
     ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1' OR symbol='$1'")
     SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
     NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
   fi
-  TYPE=$($PSQL "SELECT type FROM properties INNER JOIN types USING (type_id) WHERE atomic_number=$ATOMIC_NUMBER")
-  ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
-  MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
-  BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
-
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  #if data is not in db, show message
+  if [[ -z $SYMBOL ]]
+  then
+    echo "I could not find that element in the database."   
+  else
+  #get type, atomic mass, melting and boiling points from db
+    TYPE=$($PSQL "SELECT type FROM properties INNER JOIN types USING (type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+    ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+  #show message
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
 fi
